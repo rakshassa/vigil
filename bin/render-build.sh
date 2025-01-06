5697c92b150b0dc5936baea5737c463152a6b570
Elijah Hall <rakshassa@gmail.com>
2025-01-06 07:25:20 -0700

    render db settings

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,11 +3,11 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
 
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
 
-bundle exec rails db:migrate
+bundle exec rake db:drop db:create db:migrate db:seed
